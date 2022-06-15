3c98c844aed99b3ccf3a96309b0acf845cf039a6
cobramostar <71292042+cobramostar@users.noreply.github.com>
2022-06-15 16:32:17 +0200

    Update element_and_index_from_list_and_tuple.py

--- element_and_index_from_list_and_tuple.py
+++ element_and_index_from_list_and_tuple.py
@@ -30,6 +30,24 @@ def readListElementAndIndex(mylist):
 foo = (8, 1 , 2 , 3 , 1 , 2 , 3 , "a" , "b" , "c" , "a" , "b" , "c" , 1 , 2 , 3 , 1 , 2 , 3 , 21)
 
 readListElementAndIndex(foo)
+
+
+
+'''
+
+>>> %Run Find_an_index_of_duplicate_elements_in_list_Python_Example.py
+
+element 8 have index 0 
+element 1 have index 1 4 13 16 
+element 2 have index 2 5 14 17 
+element 3 have index 3 6 15 18 
+element a have index 7 10 
+element b have index 8 11 
+element c have index 9 12 
+element 21 have index 19 
+>>> 
+
+'''
     
 
     
