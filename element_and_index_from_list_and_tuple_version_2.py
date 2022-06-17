fc3f60a4ff32ca5cbf669b5fb81ad73ab7c18ad9
cobramostar <71292042+cobramostar@users.noreply.github.com>
2022-06-17 13:17:50 +0200

    Update element_and_index_from_list_and_tuple_version_2.py

--- element_and_index_from_list_and_tuple_version_2.py
+++ element_and_index_from_list_and_tuple_version_2.py
@@ -16,3 +16,17 @@ for i,element in enumerate(foo):
 print("     " ,"element" ,"\t","index")        
 for element, index in dictionary_list.items():
     print("\t" ,element ,"\t",index)
+
+'''
+
+>>> %Run pe1_summary_test_1_Item_2_35_tuple_index_default_dictionary.py
+      element 	 index
+	 1 	 [0, 3, 12, 15]
+	 2 	 [1, 4, 13, 16]
+	 3 	 [2, 5, 14, 17]
+	 a 	 [6, 9]
+	 b 	 [7, 10]
+	 c 	 [8, 11]
+>>> 
+
+'''
